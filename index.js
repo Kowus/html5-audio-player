b88bbd312cdd18de41f25c8b8a837106d775be9c
Kowus <barnabas@elite-education.org>
2019-07-25 16:59:32 +0000

    one file blitz

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-class AudioPlayer {
-  constructor(config) {
-    this.selector = config.selector;
-    this.keyBindings = config.keyBindings;
+(function() {
+  class AudioPlayer {
+    constructor(config) {
+      if (!window.HTMLAudioElement) {
+        console.debug('your browser does not support html5 audio');
+        this.supportsAudio = false;
+      } else {
+        this.supportsAudio = true;
+        console.debug('browser supports html5 audio');
+      }
+      this.selector = config.selector;
+      this.keyBindings = config.keyBindings;
+      this.playlist = config.playlist || [];
+      this.nowPlaying = config.nowPlaying;
+    }
+
+    get getAudioPlayerInstance() {
+      return this;
+    }
+
+    init() {
+      const selector = this.selector;
+      var audioPlayer = document.getElementById(selector);
+      this.domAudio = audioPlayer;
+    }
+    play() {
+      var audioPlayer = this.domAudio;
+      if (!this.playlist && !this.supportsAudio) return;
+    }
+    updatePlaylist() {
+      
+    }
   }
-}
+
+  window.AudioPlayer = AudioPlayer;
+})();
